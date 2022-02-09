89a142089845918e16f3e945f7a3c21c2cb7ffb8
Zapadlo <zapadlo_13@mail.ru>
2022-02-09 14:32:51 +0300

    add API users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
+import UserList from "./components/User.js";
+
 
 class App extends React.Component {
+
     constructor(props) {
         super(props)
         this.state = {
@@ -10,10 +14,41 @@ class App extends React.Component {
         }
     }
 
+    componentDidMount() {
+        // const users = [
+        //     {
+        //         'username': 'Фёдор',
+        //         'first_name': 'Фёдор',
+        //         'last_name': 'Достоевский',
+        //         'email': 1821
+        //     },
+        //     {
+        //         'username': 'Александр',
+        //         'first_name': 'Александр',
+        //         'last_name': 'Грин',
+        //         'email': 1880
+        //     },
+        // ]
+        // this.setState(
+        //     {
+        //         'users': users
+        //     }
+        // )
+
+
+         axios.get('http://127.0.0.1:8000/api/users/').then(response => {
+            // const authors = response.data
+            this.setState({
+                'users': response.data
+            })
+        }).catch(error => console.log(error))
+
+    }
+
     render() {
         return (
             <div>
-                Main App
+                <UserList users={this.state.users}/>
             </div>
         )
     }
--- todo/settings.py
+++ todo/settings.py
@@ -125,5 +125,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'usersapp.User'
 
 CORS_ALLOWED_ORIGINS = [
-   "http://localhost:3000",
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
