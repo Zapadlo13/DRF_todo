ac2e8c3291c8b032a493184eea3ff6286a05e394
Zapadlo <zapadlo_13@mail.ru>
2022-02-14 23:11:11 +0300

    add serializers,views Project and ToDo

--- usersapp/serializers.py
+++ usersapp/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
+
+from project.models import Project, ToDo
 from .models import User
 
 
 class UserModelSerializer(HyperlinkedModelSerializer):
-   class Meta:
-       model = User
-       fields = ('username', 'first_name', 'last_name', 'email',)
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email',)
+
+
+class ProjectModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('__all__',)
+
+
+class ToDoModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = ('__all__',)
--- usersapp/views.py
+++ usersapp/views.py
@@ -1,9 +1,21 @@
 
 from rest_framework.viewsets import ModelViewSet
+
+from project.models import Project,ToDo
 from .models import User
-from .serializers import UserModelSerializer
+from .serializers import UserModelSerializer,ProjectModelSerializer,ToDoModelSerializer
 
 
 class UserModelViewSet(ModelViewSet):
    queryset = User.objects.all()
    serializer_class = UserModelSerializer
+
+
+class ProjectModelViewSet(ModelViewSet):
+   queryset = Project.objects.all()
+   serializer_class = ProjectModelSerializer
+
+
+class ToDoModelViewSet(ModelViewSet):
+   queryset = ToDo.objects.all()
+   serializer_class = ToDoModelSerializer
