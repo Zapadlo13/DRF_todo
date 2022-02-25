99459eec87205c1b683fe2a31b4110121d250c11
Zapadlo <zapadlo_13@mail.ru>
2022-02-25 14:21:01 +0300

    push lesson_6

--- todo/settings.py
+++ todo/settings.py
@@ -135,6 +135,10 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
